ba8e94fb880adc217a2285e4f94945270a8bc29e
Michael <MLWALK3R@GMAIL.COM>
2013-06-19 16:03:29 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,14 @@
-  #!/bin/bash
 
 	echo -e "\e[1;34m[*] Checking connectivity\e[0m"
 	echo
 	sleep 2
-
-	# Variables
-	TESTHOST="google.com"
-	TEST=`ping -c1 $TESTHOST | grep "1 packets transmitted" | cut -d "," -f3`
-	PASS=" 0% packet loss"
-
-	if [ "$TEST" = "$PASS" ]; then
-	    echo -e "\e[1;34m[*] Congratz bro! t3h Interntz is working.\e[0m"
+    netstat -rn | grep '^default' && echo connection is up || echo connection is down
+	echo -e "\e[1;34m[*] Congratz bro! t3h Interntz is working.\e[0m"
 	sleep 3
 	echo
 	echo -e "\e[1;34m[*] Updating the cache.\e[0m"
 	echo
-	apt-get update 
+	apt-get update
 	sleep 1
 	echo -e "\e[1;34m[*] Upgrading...\e[0m"
 	sleep 2
@@ -27,6 +20,4 @@
 	echo
 	sleep 2
 	echo -e "\e[1;34m[*]All done.\e[0m"
-	else
-	    echo -e "\e[1;34m[*] You do not have an Internet connection.\e[0m"
 	fi
