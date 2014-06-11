d3cbaacdd1a1e2b3ca8be1fc61d0c0534f167ceb
Michael Walker <mlwalk3r@gmail.com>
2014-06-11 03:36:00 +0100

    Removed Checking connectivity, Shorted the cleaning commands, tweaked echo.

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -e "\E[33;40m[*]Checking if you're root\e[0m"
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
+   echo "\E[33;40mThis script must be run as root\e[0m"
 
    else echo "You're root great"
 
@@ -17,17 +17,6 @@ else
     echo -e "\E[33;40m no supported distribution found running\e[0m "
 exit 0
 fi
-echo -e "\E[33;40m[*] Checking connectivity\e[0m"
-echo
-sleep 2
-# Variables
-TESTHOST="google.com"
-TEST=`ping -c1 $TESTHOST | grep "1 packets transmitted" | cut -d "," -f3`
-PASS=" 0% packet loss"
-if [ "$TEST" = "$PASS" ]; then
-	echo -e "\E[32;40m[*] Congratz bro! t3h Interntz is working.\e[0m"
-	sleep 3
-	echo
 	sleep 1
 	if [ "$DISTRO" = "DEBIAN" ]; then
 		echo -e "\E[33;40m[*] Updating the cache.\e[0m"
@@ -35,16 +24,12 @@ if [ "$TEST" = "$PASS" ]; then
 		apt-get update
 		sleep 1
 		echo -e "\E[33;40m[*] Cleaning Up!...\e[0m"
-		apt-get install -f -y
-		sleep 1
-		apt-get autoremove -y
-		sleep 1
-		apt-get autoclean -y
+		apt-get install -f -y;apt-get autoremove -y;apt-get autoclean -y
 		sleep 1
 		echo -e "\E[33;40m[*] Upgrading...\e[0m"
 		sleep 2
 		apt-get upgrade -y
-		echo -e "\E[33;40m[*] Distribution Upgrades.. This may take a while. \e[0m"
+		echo -e "\E[33;40m[*] Distribution Upgrades.. This could take a while. \e[0m"
 		sleep 1
 		echo
 		apt-get dist-upgrade -y
